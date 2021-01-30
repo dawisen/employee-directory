3b85c57fea3934830bd7b6f3f981645258152289
gudetama <daniellewwise@gmail.com>
2021-01-29 21:37:16 -0500

    adding search input component

---
+++ src/components/searchbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./search.css"
+const Searchbar = () => {
+  return (
+  <div className='search'>
+    <input type="text" placeholder="Search"/>
+  </div>
+  );
+};
+
+export default Searchbar;
