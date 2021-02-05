dc41a2ae1accb6207ceb32d6827cdc93b7ec7b1d
gudetama <daniellewwise@gmail.com>
2021-02-05 13:52:25 -0500

    added handleSearch func to onChange event

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./search.css"
-const Searchbar = () => {
+const Searchbar = ({handleSearch}) => {
   return (
   <div className='search'>
-    <input type="text" placeholder="Search"/>
+      <input type="text" placeholder="Search" onChange={event => { handleSearch(event.target.value) }}/>
   </div>
   );
 };
 
 export default Searchbar;
+
+
+
+
