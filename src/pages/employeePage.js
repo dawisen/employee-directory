432e8266eff94109d6d86fc55d2b013ecbf36f82
gudetama <daniellewwise@gmail.com>
2021-02-05 13:56:22 -0500

    added handleSearch func to allow table filtering by searchbar value

--- src/pages/employeePage.js
+++ src/pages/employeePage.js
@@ -1,40 +1,56 @@
 import React, { useState, useEffect } from "react";
 import Container from "../components/container";
-// import Col from "../components/col";
 import Row from "../components/row";
 import Header from "../components/header";
 import getEmployees from "../api/API";
-import Searchbar from "../components/searchbar"
+import Searchbar from "../components/searchbar";
 import Table from "../components/table"
 
-function Employees() {
+function EmployeePage() {
   const [employees, setEmployees] = useState([]);
-
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
   useEffect(() => {
-      loadEmployees();
+    loadEmployees();
   }, []);
 
-    async function loadEmployees() {
-      try {
-        const employees = await getEmployees();
-        console.log("employees.data.results: ", employees.data.results);
-        setEmployees(employees.data.results);
-      } catch (error) {
-        console.log(error);
-      }
+  async function loadEmployees() {
+    try {
+      const employees = await getEmployees();
+      // console.log("employees.data.results: ", employees.data.results);
+      setEmployees(employees.data.results);
+      setFilteredEmployees(employees.data.results)
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  const handleSearch = (search) => {
+    // console.log("you searched: ", search);
+    const employeeFilter = employees.filter((person) => {
+      if (
+        person.name.first.toLowerCase().includes(search.toLowerCase()) ||
+        person.name.last.toLowerCase().includes(search.toLowerCase())
+      ) {
+        return person;
+      }
+    });
+    // console.log('FILTERED ARRAY AFTER SEARCH', employeeFilter);
+    setFilteredEmployees(employeeFilter);
+  }
 
   return (
     <div>
-    <Header />
+      <Header />
       <Container fluid="true">
-      <Searchbar />
-      <Row>
-          <Table employees={employees}/>
-      </Row>
+        <Searchbar handleSearch={handleSearch}/>
+        <Row>
+          <div class='table-responsive'>
+          <Table employees={filteredEmployees} />
+          </div>
+        </Row>
       </Container>
     </div>
   );
 }
 
-export default Employees;
+export default EmployeePage;
